f1fa9377118794d844822f1652a5c2b01938bb03
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-04-15 11:04:46 -0300

    Update info at Home page

--- partials/front-page/home-data.php
+++ partials/front-page/home-data.php
@@ -4,15 +4,21 @@
       <div class="row display-flex-center">
           <div class="home__data--col-1 col-md-12 col-lg-6 col-padding-small-xs col-sm-padding-large bg-color-2 white-text">
               <h3 class="">Achou que esse ano não teríamos FLISol?</h3>
+                <p>Devido a Pandemia do COVID-19 precisamos pensar em um novo formato para o evento</p>
               <p>
-                  Trabalhamos muito para trazer a vocês o evento totalmente online </p>
+                  Trabalhamos muito para trazer a vocês o evento online </p>
           </div>
           <div class="home__data--col-2 col-md-12 col-lg-6 flexbox-strech center-xs middle-xs no-gutter-col">
                 <div class="white-text shadow-large col-padding-small-xs col-sm-padding-large overlap-left-large flexbox middle-xs center-xs right-bkg bg-color-4 content-date">
                     <h2>24 Abr 2021</h2>
-                    <h5>09:00h às 17:00h</h5>
+                    <h6>Palestras e Atividades</h6>
+                    <h5>10:00h às 21:00h</h5>
                     <h4>Online</h4>
-                    <h6>Devido a Pandemia do COVID-19 nosso evento será inteiramente online esse ano</h6>
+                   <!--  <h6>Devido a Pandemia do COVID-19 nosso evento será online esse ano</h6> -->
+                    <h6>Formatação e instalação de softwares LIVRES e Coleta de resíduos eletrônicos</h6>
+                    <h5>13:00h às 17:00h</h5>
+                    <h4>Campus Univille São Bento do Sul</h4>
+                    <p><small>Lembrando que serão seguidos todos os protocolos de segurança.</small></p>
                 </div>
           </div>
         </div>
